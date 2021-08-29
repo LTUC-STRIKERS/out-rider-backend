0da96c96589471611dbf8d7452d9d0f2283bc2db
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-08-29 23:55:27 +0300

    jj

--- server.js
+++ server.js
@@ -5,18 +5,16 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const axios = require('axios');
 require('dotenv').config();
-
-
-const PORT = process.env.PORT;
-
 const server = express();
 server.use(cors());
 server.use(express.json());
 const yelp = require('yelp-fusion');
+const PORT = process.env.PORT;
+
 // const caching = require('./caching')
 
 //database
-// mongoose.connect(`${process.env.MONGODB_LINK}`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`${process.env.MONGODB_LINK}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //localhost:3004/search?term=food&location=london,uk 
 const getSearch = (req, res) => {
@@ -43,6 +41,88 @@ const getSearch = (req, res) => {
 
 
 server.get('/search', getSearch);
+
+
+//Schema
+const favResturantSchema = new mongoose.Schema({
+    name: String,
+    rating:Number,
+    image_url:String,
+    email:String,
+    phone:Number,
+    review_count:Number,
+
+
+});
+
+//Model
+const favResturantModel = mongoose.model('Restaurant', favResturantSchema);
+
+
+function seedDataCollection() {
+    const Restaurant1 = new favResturantModel({
+        name: "nammmme",
+        rating:5,
+        image_url:'image_url',
+        email:'email',
+        phone:6666,
+        review_count:555,
+    })
+
+    const Restaurant2 = new favResturantModel({
+        name: "nammmme",
+        rating:5,
+        image_url:'image_url',
+        email:'email',
+        phone:6666,
+        review_count:555,
+    })
+
+
+    const Restaurant3 = new favResturantModel({
+        name: "nammmme",
+        rating:5,
+        image_url:'image_url',
+        email:'email',
+        phone:6666,
+        review_count:555,
+    })
+
+
+    const Restaurant4 = new favResturantModel({
+        name: "nammmme",
+        rating:5,
+        image_url:'image_url',
+        email:'email',
+        phone:6666,
+        review_count:555,
+    })
+
+    Restaurant1.save();
+    Restaurant2.save();
+    Restaurant3.save();
+    Restaurant4.save();
+
+}
+// seedDataCollection();
+
+server.get('/favoriteplace', getfavoriteplace);
+function getfavoriteplace(request,response) {
+
+    let email = request.query.email;
+
+    favResturantModel.find({email:email},function(err,ownerData){
+
+        if(err){
+            console.log('Error in getting data')
+        }else {
+            console.log(ownerData);
+            response.send(ownerData)
+        }
+
+    })
+
+}
 
 class Restaurant {
     constructor(restaurant) {
